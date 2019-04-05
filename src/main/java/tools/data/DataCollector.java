b156c4beff72e9cc55bad8327df3a613f8c175aa
Марк <mark.karlinsky.04@mail.ru>
2019-04-05 21:35:17 +0500

    Исправлен баг с кодировками. Программа больше не зависит от кодировки системы.

--- src/main/java/tools/data/DataCollector.java
+++ src/main/java/tools/data/DataCollector.java
@@ -4,12 +4,13 @@ import tools.data.types.Text;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class DataCollector {
-    private int sampleCount;
+    private final int sampleCount;
 
-    private Text[] samples;
+    private final Text[] samples;
 
     public DataCollector(int sampleCount) {
         this.sampleCount = sampleCount;
@@ -21,7 +22,7 @@ public class DataCollector {
             File sample = new File(String.format("trainingData/sample%d.txt", i + 1));
             if (sample.exists()) {
                 try {
-                    samples[i] = new Text(new String(Files.readAllBytes(sample.toPath())));
+                    samples[i] = new Text(new String(Files.readAllBytes(sample.toPath()), StandardCharsets.UTF_8));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
